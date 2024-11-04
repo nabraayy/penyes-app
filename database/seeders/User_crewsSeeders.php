e24a35833c6cd6b94894f2e88f1ef235dbd7fbd8
Nay <garciatarraganayara@gmail.com>
2024-11-04 19:11:11 +0100

    update

---
+++ database/seeders/User_crewsSeeders.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class User_crewsSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
